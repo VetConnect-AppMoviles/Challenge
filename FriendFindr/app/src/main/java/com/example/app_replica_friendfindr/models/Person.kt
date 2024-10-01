812171617742104c2e18228b58574b1c0a6365e1
Rodrigo Salvador <u202213646@upc.edu.pe>
2024-09-30 21:58:27 -0500

    feat: add Person.kt

---
+++ FriendFindr/app/src/main/java/com/example/app_replica_friendfindr/models/Person.kt
@@ -0,0 +1,27 @@
+package com.example.app_replica_friendfindr.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Person (
+    @PrimaryKey
+    val id: Int? = null,
+
+    @ColumnInfo(name = "first_name")
+    val firstName: String?,
+
+    @ColumnInfo(name = "last_name")
+    val lastName: String?,
+
+    @ColumnInfo(name = "email")
+    val email: String?,
+
+    @ColumnInfo(name = "cell")
+    val cell: String?,
+
+    @ColumnInfo(name = "picture")
+    val picture: String?,
+)
